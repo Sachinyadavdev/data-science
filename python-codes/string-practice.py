5f9a01e761a83adc38ac5a2ad318844fc1862a9f
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-09-21 22:39:02 +0530

    Updated the Code

---
+++ python-codes/string-practice.py
@@ -0,0 +1,10 @@
+name = "Alice"
+
+print(name)
+
+para = """ This is the paragraph name 
+as the python interpreter and the anme of this is known as the best python game"""
+
+print(para)
+# name[1] = "T" string are immutable and can not be changed.
+print(name[0])
