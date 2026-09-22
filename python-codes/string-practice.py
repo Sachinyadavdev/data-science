1ce6382e00963e8cbe6ff3537afe4475eded4a55
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-09-22 10:48:45 +0530

    Update the Code

--- python-codes/string-practice.py
+++ python-codes/string-practice.py
@@ -1,10 +1,10 @@
 name = "Alice6"
 
 
-para = """This is the paragraph name 
-as the python interpreter and the anme of this is known as the is best python game"""
+# para = """This is the paragraph name 
+# as the python interpreter and the anme of this is known as the is best python game"""
 
-print(para)
+# print(para)
 # name[1] = "T" string are immutable and can not be changed.
 # print(name[0])
 
@@ -12,22 +12,35 @@ print(para)
 # name.lower()
 # print(name)
 
-print(para.strip()) 
+# print(para.strip()) 
 
 # print(para.replace("This", "Mai hu na")) 
-print(para.split()) # It will make the split the string into the list
+# print(para.split()) # It will make the split the string into the list
 
-list_name = ['This', 'is','name']
+# list_name = ['This', 'is','name']
 
-print(name.join(list_name))
+# print(name.join(list_name))
 
 
-print(para.find('is')) # Find the Occurence of the string 
-print(para.count('this')) # This function will find the number of occurence and it is case sensitive 
+# print(para.find('is')) # Find the Occurence of the string 
+# print(para.count('this')) # This function will find the number of occurence and it is case sensitive 
 
-print(para.startswith('This')) # This will return the value of True and False if it retuen the correct and false statement.
+# print(para.startswith('This')) # This will return the value of True and False if it retuen the correct and false statement.
 
-print(para.isdigit())
-print(name.isalpha())
+# print(para.isdigit())
+# print(name.isalpha())
 
-print(name.isalnum())
+# print(name.isalnum())
+
+text = "Data Science"
+
+# Lenght of the String 
+
+print(len(text))
+print(text.__len__())
+print(text[-1])
+print(text[5])
+
+# Reverse the String 
+
+print(text[::-1])
