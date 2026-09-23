850778f9727395380124ec0c37f57b3d50e33ac2
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-09-23 11:27:55 +0530

    Updated the Code

---
+++ python-codes/string-method.py
@@ -0,0 +1,14 @@
+# Clean User Input 
+
+name = "   Sachin Yadav   "
+
+print(name)
+print(name.strip())
+
+text = "I am learning Java"
+
+print(text.replace('Java','Python'))
+
+sentence = "Python Java SQL React HTML"
+
+print(sentence.split())
