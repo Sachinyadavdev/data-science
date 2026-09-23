c7eba0aa759a070483f6f295761edf6411ee53f9
Sachin Yadav <88433447+Sachinyadavdev@users.noreply.github.com>
2026-09-23 11:58:03 +0530

    Practice the Python Strings

--- python-codes/string-method.py
+++ python-codes/string-method.py
@@ -12,3 +12,24 @@ print(text.replace('Java','Python'))
 sentence = "Python Java SQL React HTML"
 
 print(sentence.split())
+
+words = ["Python","is","easy","to","learn"]
+
+words_join = " ".join(words)
+
+print(words_join)
+
+# Check the Content 
+
+email = "sachinyadav@gmail.com"
+
+# if email.endswith('@gmail.com') and email.startswith('sachinyadav') and email.__contains__('@'):
+#     print("Valid Email")
+
+# else:
+#     print("Invalid Email Entered")
+
+print('@' in email)
+print(email.endswith('.com'))
+print(email.startswith('sachin'))
+print('gmail' in email)
